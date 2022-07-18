5f6ba75c535b8f9c90383fb4afb887088b5a038e
Anna Gevel <anngevel@gmail.com>
2022-07-18 13:31:10 +0100

    added a feature project for components

---
+++ src/Sitecore/Feature/MyAccount/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MyAccount.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
